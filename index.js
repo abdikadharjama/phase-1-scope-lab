6eef556b46cf56af94a6b56d3a81ac5097ae1ed0
abdikadharjama <abdikadharjama@gmail.com>
2024-01-25 01:19:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Declare customerName to be 'bob' in global scope
+let customerName = 'bob';
+
+// Function to modify the customerName variable to uppercase
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set a new global variable bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Implicitly global
+}
+
+// Function to overwrite the best customer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Declare a constant and try to change it, which should fail
+const leastFavoriteCustomer = 'some initial value';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'attempting to change value'; // This should throw an error
+}
